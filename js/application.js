67835fc63df1680e5bea2227d72bf1721af100c8
“mia” <mia_dacamara@Mias-MacBook-Pro.local>
2015-04-20 10:05:52 +0800

    Users: sign up working

--- js/application.js
+++ js/application.js
@@ -2,23 +2,46 @@ $ (document).ready(function(){
 
 	// SIGN UP - NEW USER
 	$('#sign-up').on('click', function(){
+		event.preventDefault(); 
+		
 		$.ajax({
 	    type: 'POST',
-	    url: 'http://0.0.0.0:3000/users',
+	    url: 'http://localhost:3000/users',
 	    data: {
-	    	newUser: {
+	    	user: {
 	    		username: $('#username').val(),
 	    		email: $('#email').val(),
-	    		password: $('#password').val()
+	    		password: $('#password_signup').val()
 	    	}
 	    },
 	    dataType: 'json',
 	    success: function(response){
+	    	console.log(response);
 	    	alert('Good Job! Sign up successful');
 	    }
 		});
 	});
 
-	// SIGN IN - NEW SESSION
+	// LOG IN - NEW SESSION
+	$('#log-in').on('click', function(){
+		console.log('testing log-in button')
+		event.preventDefault(); 
+
+		$.ajax({
+	    type: 'POST',
+	    url: 'http://localhost:3000/sessions',
+	    data: {
+	    	user: {
+	    		username: $('#username_signin').val(),
+	    		password: $('#password_signin').val()	
+	    	}
+	    },
+	    dataType: 'json',
+	    success: function(response){
+	    	console.log(response);
+	    	alert('Good Job! Sign in successful');
+	    }
+		});
+	});
 
 });
